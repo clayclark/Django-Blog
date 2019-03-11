9a7aec326478d6ebeb383f450ac78f9129e29115
Clay Clark <clayclark23@gmail.com>
2019-03-10 21:38:39 -0700

    Use SQL Post data

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post 
 
-posts = [
-    {
-        'author': 'Clay',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 23, 2019'
-    },
-    {
-        'author': 'Chlo',
-        'title': 'Blog Post 2',
-        'content': '2 post content',
-        'date_posted': 'January 1, 2019'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
